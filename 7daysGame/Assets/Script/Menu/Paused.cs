cf12b5873bc15142c00575db6c96c6ba18132243
KevinVLesni <krihagosickmatron@gmail.com>
2023-02-10 11:00:16 +0300

    Pause & Menu
    ►Добавил меню паузы в игре, затемнение экрана с выбором перехода в главное меню или продолжения игры. Во время этого меню функция в коде останавливает время, однако лук всё равно крутится за курсором.

--- 7daysGame/Assets/Script/Menu/Paused.cs
+++ 7daysGame/Assets/Script/Menu/Paused.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Paused : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject pause;
     void Start()
     {
-        
+        pause.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void PauseOff()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
 }
