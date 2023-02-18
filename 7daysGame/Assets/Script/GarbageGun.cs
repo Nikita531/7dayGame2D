0e8f951869a97cbe39c43c5d10db7348ecab5331
Phagos-pop <zaroxsino@gmail.com>
2023-02-18 17:42:26 +0300

    Change 1 Level

--- 7daysGame/Assets/Script/GarbageGun.cs
+++ 7daysGame/Assets/Script/GarbageGun.cs
@@ -7,6 +7,8 @@ public class GarbageGun : MonoBehaviour
     [SerializeField] private List<GarbageBullet> garbagePrefabs;
     [SerializeField] private float power;
 
+    private int lastRN;
+
     private Camera mainCamera;
 
     private void Start()
@@ -18,14 +20,23 @@ public class GarbageGun : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            float enter;
-            Vector3 pos = Input.mousePosition;
-            //Ray ray = mainCamera.ScreenPointToRay(pos);
-            Vector2 speed = (pos - transform.position) * power;
-            int number = Random.Range(0, garbagePrefabs.Count);
-            Rigidbody2D garbageBullet = Instantiate(garbagePrefabs[number], transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
-            garbageBullet.AddForce(speed, ForceMode2D.Impulse);
-
+            GarbageShot();
         }
+    }
+
+    private void GarbageShot()
+    {
+        Vector3 pos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 speed = (pos - transform.position) * power;
+        
+        int number;
+        do
+        {
+            number = Random.Range(0, garbagePrefabs.Count);
+        } while (number == lastRN);
+        lastRN = number;
+
+        Rigidbody2D garbageBullet = Instantiate(garbagePrefabs[number], transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        garbageBullet.AddForce(speed, ForceMode2D.Impulse);
     }
 }
